e14b3ea10278dd86a5035ef5a9d76b405cbaf0ee
klassanov <klassanov@gmail.com>
2018-10-14 18:26:12 +0300

    KLS - Added folder migrations

---
+++ CustomersAgenda.DataAccess/Migrations/201810131808165_Initial.cs
@@ -0,0 +1,45 @@
+namespace CustomersAgenda.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        DueAmount = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        BirthDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Payments",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        CustomerId = c.Int(nullable: false),
+                        Amount = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Customers", t => t.CustomerId, cascadeDelete: true)
+                .Index(t => t.CustomerId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Payments", "CustomerId", "dbo.Customers");
+            DropIndex("dbo.Payments", new[] { "CustomerId" });
+            DropTable("dbo.Payments");
+            DropTable("dbo.Customers");
+        }
+    }
+}
